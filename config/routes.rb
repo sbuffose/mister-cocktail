26eb6c83f1e25b02414af002d11dc4631e4dd63a
Sebastian Buffo Sempe <sebastianbuffosempe@gmail.com>
2021-02-20 09:38:16 -0300

    setup project

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  root to: "cocktails#index"
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create]
+    resources :reviews, only: [:create]
+  end
+  resources :doses, only: [:destroy]
+end
